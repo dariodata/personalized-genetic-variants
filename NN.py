ae78bc44f81bb9272fc9fb960d9f58bc0c8a7364
Dario Arcos-Diaz <dario86@gmail.com>
2017-09-13 17:06:56 +0200

    add L2 regularization

--- NN.py
+++ NN.py
@@ -276,6 +276,10 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0001,
     # Cost function: Add cost function to tensorflow graph
     cost = compute_cost(Z4, Y)
     # cost = compute_cost_multiclasslogloss(Z4, Y)
+    regularizers = tf.nn.l2_loss(parameters['W1']) + tf.nn.l2_loss(parameters['W2']) + tf.nn.l2_loss(parameters['W3']) \
+                   + tf.nn.l2_loss(parameters['W4'])
+    beta = 0.001
+    cost = tf.reduce_mean(cost + beta*regularizers)
 
     # Backpropagation: Define the tensorflow optimizer. Use an AdamOptimizer.
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
